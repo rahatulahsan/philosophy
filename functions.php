ed37df5233b62112744ef38fb3ad79d91444515e
rahatulahsan <rahatulahsan@gmail.com>
2023-07-16 21:45:00 +0600

    Homepage Pagination

--- functions.php
+++ functions.php
@@ -35,3 +35,21 @@ function philosophy_assets(){
 }
 
 add_action('wp_enqueue_scripts', 'philosophy_assets');
+
+
+// HOMEPAGE PAGINATION SETUP
+function philosophy_pagination(){
+
+    global $wp_query;
+    $links =  paginate_links( array(
+        'current' => max(1, get_query_var( 'paged' )),
+        'total' => $wp_query->max_num_pages,
+        'type' => 'list'
+    ) );
+
+    $links = str_replace('page-numbers', 'pgn__num', $links);
+    $links = str_replace("<ul class='pgn__num'>", "<ul>", $links);
+    $links = str_replace('next pgn__num', 'pgn__next', $links);
+    $links = str_replace('prev pgn__num', 'pgn__prev', $links);
+    echo $links;
+}
